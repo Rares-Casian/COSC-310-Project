d6fe465bf363430ab8f45c9420ad7c44c43abd78
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2025-11-04 08:14:39 -0800

    Added more authentication schemas

--- backend/authentication/schemas.py
+++ backend/authentication/schemas.py
@@ -28,3 +28,32 @@ class TokenData(BaseModel):
     class Config:
         allow_population_by_field_name = True
         use_enum_values = True
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    role: UserRole = UserRole.member
+    status: UserStatus = UserStatus.ACTIVE
+
+
+class UserResponse(BaseModel):
+    user_id: str
+    username: str
+    email: EmailStr
+    role: str
+    status: str
+
+    class Config:
+        use_enum_values = True
+
+
+class UserToken(BaseModel):
+    user_id: str
+    username: str
+    email: EmailStr
+    role: str
+    status: str
+
+    class Config:
+        use_enum_values = True
